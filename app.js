1642df70600cea055183f285cd7de5e51d9ca017
IgordeMoura <igormoura14@yahoo.com.br>
2024-04-02 20:40:20 -0300

    Banco de Dados Não Relacional - Firebase App Libri - Igor

---
+++ app.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAZNq1761gFmrsC1VGufPbprFiWDYOuA3U",
+    authDomain: "app-libri-igor-de-moura-6ae03.firebaseapp.com",
+    projectId: "app-libri-igor-de-moura-6ae03",
+    storageBucket: "app-libri-igor-de-moura-6ae03.appspot.com",
+    messagingSenderId: "118285003868",
+    appId: "1:118285003868:web:8b9c16e72530fd4ef34a99",
+    measurementId: "G-PJQRDWXFWC"
+  };

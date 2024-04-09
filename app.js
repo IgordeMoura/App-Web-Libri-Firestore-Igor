0607ef6e29f5abaa21f264979529902f8a56d2dc
IgordeMoura <igormoura14@yahoo.com.br>
2024-04-09 20:35:32 -0300

    conexão firebase - igor

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAZNq1761gFmrsC1VGufPbprFiWDYOuA3U",
-    authDomain: "app-libri-igor-de-moura-6ae03.firebaseapp.com",
-    projectId: "app-libri-igor-de-moura-6ae03",
-    storageBucket: "app-libri-igor-de-moura-6ae03.appspot.com",
-    messagingSenderId: "118285003868",
-    appId: "1:118285003868:web:8b9c16e72530fd4ef34a99",
-    measurementId: "G-PJQRDWXFWC"
-  };
+db.collection('libri-firestore')
+  .get()
+  .then(
+    (snapshot)=>{
+      // console.log(snapshot.docs)
+      snapshot.docs.forEach(doc => {
+        console.log(doc.data());
+      });
+    }
+  );
